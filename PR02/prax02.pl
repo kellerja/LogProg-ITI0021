e436516ad27dedd8ad6febfd8893c187f5da4179
jakell <jakell@ttu.ee>
2017-09-12 14:18:11 +0300

    fathers side of the family

--- PR02/prax02.pl
+++ PR02/prax02.pl
@@ -18,11 +18,15 @@ brother(Person, Brother) :-
     mother(Person, Mother) , mother(Brother, Mother) , Person \= Brother , male(Brother).
 sister(Person, Sister) :- 
     mother(Person, Mother) , mother(Sister, Mother) , Person \= Sister , female(Sister).
-aunt(Person, Aunt) :- 
+aunt(Person, Aunt) :- % mother's side
     mother(Person, Mother) , sister(Mother, Aunt).
+aunt(Person, Aunt) :- % father's sides
+    mother(Person, Mother), married(Mother, Father), sister(Father, Aunt).
 uncle(Person, Uncle) :- 
     aunt(Person, Aunt) , married(Aunt, Uncle).
 grandfather(Person, Grandfather) :- 
     grandmother(Person, Grandmother) , married(Grandmother, Grandfather).
-grandmother(Person, Grandmother) :- 
+grandmother(Person, Grandmother) :- % mother's side
     mother(Person, Mother) , mother(Mother, Grandmother).
+grandmother(Person, Grandmother) :- % father's side
+    mother(Person, Mother) , married(Mother, Father) , mother(Father, Grandmother).
