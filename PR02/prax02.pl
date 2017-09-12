22a25432e1d68e564cbc098b7fb799d15d90b79e
jakell <jakell@ttu.ee>
2017-09-12 12:54:16 +0300

    fix relatives

--- PR02/prax02.pl
+++ PR02/prax02.pl
@@ -1,13 +1,26 @@
-married(anne,vello).
-mother(evelin,anne).
 female(anne).
 female(evelin).
+female(mari).
+male(priit).
 male(vello).
+male(tiit).
+married(anne,vello).
+mother(evelin,anne).
+mother(mari, anne).
+merried(evelin, tiit).
+mother(priit, evelin).
 
-father(Child, Father) :- mother(Child, Mother) , married(Mother, Father).
-brother(Person, Brother) :- male(Person) , mother(Person, Mother) , mother(Brother, Mother).
-sister(Person, Sister) :- female(Person) , mother(Person, Mother) , mother(Sister, Mother).
-aunt(Person, Aunt) :- female(Aunt) , grandmother(Aunt, Grandmother) , mother(Person, Mother) , grandmother(Mother, Grandmother).
-uncle(Person, Uncle) :- male(Uncle) , grandmother(Uncle, Grandmother) , mother(Person, Mother) , grandmother(Mother, Grandmother).
-grandfather(Person, Grandfather) :- mother(Person, Mother) , mother(Mother, Grandmother) , married(Grandmother, Grandfather).
-grandmother(Person, Grandmother) :- mother(Person, Mother) , mother(Mother, Grandmother).
+father(Child, Father) :- 
+    mother(Child, Mother) , married(Mother, Father).
+brother(Person, Brother) :- 
+    Person \= Brother , male(Person) , mother(Person, Mother) , mother(Brother, Mother).
+sister(Person, Sister) :- 
+    Person \= Sister , female(Person) , mother(Person, Mother) , mother(Sister, Mother).
+aunt(Person, Aunt) :- 
+    female(Aunt) , mother(Aunt, Grandmother) , grandmother(Person, Grandmother).
+uncle(Person, Uncle) :- 
+    male(Uncle) , married(Uncle, Aunt) , aunt(Person, Aunt).
+grandfather(Person, Grandfather) :- 
+    grandmother(Person, Grandmother) , married(Grandmother, Grandfather).
+grandmother(Person, Grandmother) :- 
+    mother(Person, Mother) , mother(Mother, Grandmother).
