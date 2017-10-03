b4e6a72c9dd4edd1e42b98deacc2a0b0079750ae
jakell <jakell@ttu.ee>
2017-10-03 17:58:07 +0300

    remove kordista

--- PR05/prax05.pl
+++ PR05/prax05.pl
@@ -19,14 +19,3 @@ paki([A, B|List], [A|X]) :-
 duplikeeri([], []).
 duplikeeri([H|T],[H, H|X]) :-
     duplikeeri(T, X).
-
-kordista_element(A, 1, []).
-kordista_element(A, N, [A|Result]) :-
-    Z is N - 1,
-    kordista_element(A, Z, Result), !.
-
-kordista([], _, []).
-kordista([H|T], N, [H|X]) :-
-    kordista_element(H, N, Result),
-    kordista(T, N, Y),
-    append(Result, Y, X), !.
