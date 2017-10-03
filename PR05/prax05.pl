646bd08ed98590b86b8abed2c96523701ee7c6bf
jakell <jakell@ttu.ee>
2017-10-03 13:25:51 +0300

    suurim ja paki tühi

--- PR05/prax05.pl
+++ PR05/prax05.pl
@@ -2,12 +2,14 @@ viimane_element(A, [A]) :- !.
 viimane_element(X,[_|L]) :-
     viimane_element(X, L).
 
+suurim([], []).
 suurim([A], [A]).
 suurim([A, B|List], [A|X]) :-
     A > B, suurim([B|List], X), !.
 suurim([A, B|List], [B|X]) :-
     B > A, suurim([B|List], X).
 
+paki([], []).
 paki([B], [B]).
 paki([A, B|List], X) :-
     A = B, paki([B|List], X), !.
