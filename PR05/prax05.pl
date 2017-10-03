c8bad03461ab4394958144ff07fd90dc6960a971
jakell <jakell@ttu.ee>
2017-10-03 17:59:43 +0300

    without duplikeeri

--- PR05/prax05.pl
+++ PR05/prax05.pl
@@ -16,6 +16,13 @@ paki([A, B|List], X) :-
 paki([A, B|List], [A|X]) :-
     not(A = B), paki([B|List], X).
 
-duplikeeri([], []).
-duplikeeri([H|T],[H, H|X]) :-
-    duplikeeri(T, X).
+kordista_element(A, 1, []).
+kordista_element(A, N, [A|Result]) :-
+    Z is N - 1,
+    kordista_element(A, Z, Result), !.
+
+kordista([], _, []).
+kordista([H|T], N, [H|X]) :-
+    kordista_element(H, N, Result),
+    kordista(T, N, Y),
+    append(Result, Y, X), !.
