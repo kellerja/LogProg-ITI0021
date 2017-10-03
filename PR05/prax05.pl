19daf1184da4532d37e32df07e4ac3596ce35532
jakell <jakell@ttu.ee>
2017-10-03 13:24:26 +0300

    prax05

---
+++ PR05/prax05.pl
@@ -0,0 +1,30 @@
+viimane_element(A, [A]) :- !.
+viimane_element(X,[_|L]) :-
+    viimane_element(X, L).
+
+suurim([A], [A]).
+suurim([A, B|List], [A|X]) :-
+    A > B, suurim([B|List], X), !.
+suurim([A, B|List], [B|X]) :-
+    B > A, suurim([B|List], X).
+
+paki([B], [B]).
+paki([A, B|List], X) :-
+    A = B, paki([B|List], X), !.
+paki([A, B|List], [A|X]) :-
+    not(A = B), paki([B|List], X).
+
+duplikeeri([], []).
+duplikeeri([H|T],[H, H|X]) :-
+    duplikeeri(T, X).
+
+kordista_element(A, 1, []).
+kordista_element(A, N, [A|Result]) :-
+    Z is N - 1,
+    kordista_element(A, Z, Result), !.
+
+kordista([], _, []).
+kordista([H|T], N, [H|X]) :-
+    kordista_element(H, N, Result),
+    kordista(T, N, Y),
+    append(Result, Y, X).
