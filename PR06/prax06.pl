1ccd60657edb5174c119ceaf65a4bc4b5917466e
jakell <jakell@ttu.ee>
2017-10-16 21:09:28 +0300

    better

--- PR06/prax06.pl
+++ PR06/prax06.pl
@@ -3,6 +3,7 @@ is_a(loom, elusolend).
     is_a(omnivoor, loom).
     is_a(karnivoor, loom).
     is_a(herbivoor, loom).
+    is_a(oistaime_sooja, loom).
 is_a(taim, elusolend).
     is_a(oistaim, taim).
     is_a(mitteoistaim, taim).
@@ -17,6 +18,7 @@ is_a(inimene, omnivoor).
 is_a(kass, karnivoor).
 is_a(kaelkirjak, herbivoor).
 is_a(rabbit, herbivoor).
+is_a(muruniiduk, oistaime_sooja).
 
 is_a(porgand, mitteoistaim).
 is_a(kapsas, oistaim).
@@ -35,6 +37,7 @@ soob(herbivoor, taim).
 soob(karnivoor, loom).
 soob(omnivoor, taim).
 soob(omnivoor, loom).
+soob(oistaime_sooja, oistaim).
 
 loomne_toit(loom).
 taimne_toit(taim).
@@ -56,8 +59,8 @@ kasvab(X) :- alamklass(X, Y), kasvab(Y).
 
 liigub(X) :- alamklass(X, Y), liigub(Y).
 
-%loomne_toit(X) :- alamhulk(X, Y), is_a(X, Y), loomne_toit(Y).
-%taimne_toit(X) :- alamhulk(X, Y), is_a(X, Y), taimne_toit(Y).
+loomne_toit(X) :- alamhulk(X, Y), is_a(X, Y), loomne_toit(Y), !.
+taimne_toit(X) :- alamhulk(X, Y), is_a(X, Y), taimne_toit(Y), !.
 
 % söömina vastavalt ajale.
 oo(Aeg) :- Aeg >= 0 , Aeg =< 6, !.
@@ -65,17 +68,17 @@ oo(Aeg) :- Aeg >= 22, Aeg =< 24, !.
 
 soob(Kes, Keda, Millal) :- 
     oo(Millal),
-    %loomne_toit(Keda),
+    loomne_toit(Keda),
     loomne_toit(Toit),
     soob(Sooja, Toit),
     alamklass(Kes, Sooja),
-    alamklass(Keda, Toit),
-    Kes \= Keda.
+    %alamklass(Keda, Toit),
+    Kes \= Keda, !.
 soob(Kes, Keda, Millal) :-
     \+ oo(Millal),
-    %taimne_toit(Keda),
+    taimne_toit(Keda),
     taimne_toit(Toit),
     soob(Sooja, Toit),
     alamklass(Kes, Sooja),
-    alamklass(Keda, Toit),
+    %alamklass(Keda, Toit),
     Kes \= Keda.
