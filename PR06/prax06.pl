23b38865345b841878144fdb1004a6b7fb1aafc1
jakell <jakell@ttu.ee>
2017-10-16 18:47:14 +0300

    soob alternative solution

--- PR06/prax06.pl
+++ PR06/prax06.pl
@@ -57,19 +57,19 @@ liigub(X) :- alamklass(X, Y), liigub(Y), !.
 loomne_toit(X) :- alamklass(X, Y), loomne_toit(Y), !.
 taimne_toit(X) :- alamklass(X, Y), taimne_toit(Y), !.
 
-soob(X, Y) :- 
-    X \= Y, loomne_toit(Y), 
-    (alamklass(X, omnivoor); alamklass(X, karnivoor)), !.
-soob(X, Y) :- 
-    X \= Y, taimne_toit(Y),
-    alamklass(X, omnivoor).
 % söömina vastavalt ajale.
 oo(Aeg) :- Aeg >= 0 , Aeg =< 6, !.
 oo(Aeg) :- Aeg >= 22, Aeg =< 24, !.
 
 soob(Kes, Keda, Millal) :- 
-    soob(Kes, Keda), 
-    oo(Millal), !.
+    oo(Millal),
+    loomne_toit(Keda),
+    loomne_toit(Toit),
+    soob(Sooja, Toit),
+    alamklass(Kes, Sooja), !.
 soob(Kes, Keda, Millal) :-
-    soob(Kes, Keda), 
-    \+ oo(Millal).
+    \+ oo(Millal),
+    taimne_toit(Keda),
+    taimne_toit(Toit),
+    soob(Sooja, Toit),
+    alamklass(Kes, Sooja), !.
