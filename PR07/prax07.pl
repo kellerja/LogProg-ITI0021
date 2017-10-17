6ab251efbc2809d3eb91d2dbdd59412c74cd7627
jakell <jakell@ttu.ee>
2017-10-17 16:15:10 +0300

    hulgad

--- PR07/prax07.pl
+++ PR07/prax07.pl
@@ -14,3 +14,19 @@ yhend(A, [X|B], [X|Result]) :-
     not(element(X, Result)), !.
 yhend(A, [X|B], Result) :-
     yhend(A, B, Result).
+
+vahe([], _, []).
+vahe([X|A], B, [X|Result]) :-
+    vahe(A, B, Result),
+    not(element(X, B)), !.
+vahe([X|A], B, Result) :-
+    vahe(A, B, Result).
+
+ristkorrutis_abi(_, [], [], _).
+ristkorrutis_abi([], [Y|B], Result, ACopy) :-
+    ristkorrutis_abi(ACopy, B, Result, ACopy), !.
+ristkorrutis_abi([X|A], [Y|B], [[X, Y]|Result], ACopy) :-
+    ristkorrutis_abi(A, [Y|B], Result, ACopy).
+
+ristkorrutis(A, B, Result) :-
+    ristkorrutis_abi(A, B, Result, A).
