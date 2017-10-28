27747ef69c87c39a74f2d1adbee331d32b02b813
jakell <jakell@ttu.ee>
2017-10-28 13:01:16 +0300

    mitte väha hea lahendus

--- PR08/prax08.pl
+++ PR08/prax08.pl
@@ -4,6 +4,8 @@ hulk(a, 5).
 hulk(a, 7).
 hulk(a, 9).
 hulk(a, 3).
+hulk(a, 24).
+hulk(a, 34).
 hulk(a, 2).
 hulk(a, -66).
 hulk(a, 0).
@@ -15,7 +17,7 @@ lendab(X) :- lind(X).
 
 :- dynamic suurim/1.
 
-suurim(X, Y) :- suurim(Y), !, Y < X, retract(suurim(Y)), assertz(suurim(X)).
+suurim(X, Y) :- suurim(Y), !, Y @< X, retract(suurim(Y)), assertz(suurim(X)).
 suurim(X, _) :- assertz(suurim(X)).
 
 max(Hulga_nimi, Max_element) :-
@@ -24,16 +26,36 @@ max(Hulga_nimi, Max_element) :-
     fail.
 max(Hulga_nimi, Max_element) :- suurim(Max_element), retract(suurim(Max_element)).
 
+:- dynamic vaikseim/1.
+vaikseim(X, Y) :- vaikseim(Y), !, Y @> X, retract(vaikseim(Y)), assertz(vaikseim(X)).
+vaikseim(X, _) :- assertz(vaikseim(X)).
+
+min(Hulk, Hulga_nimi, Min_Element) :- 
+    Term =.. [Hulk, Hulga_nimi, Min_Element],
+    Term,
+    vaikseim(Min_Element, Y),
+    fail.
+min(Hulk, Hulga_nimi, Min_Element) :- vaikseim(Min_Element), retract(vaikseim(Min_Element)).
+
 :- dynamic dynamic_hulk/2.
 set_viimane(Hulga_nimi) :- hulk(Hulga_nimi, X), retractall(viimane(Y)), assertz(viimane(X)), fail.
 loo_dynamic_hulk(Hulga_nimi) :- hulk(Hulga_nimi, X), assertz(dynamic_hulk(Hulga_nimi, X)), fail.
 
+add_to_list(Element) :- 
+    temp_hulk(Y), 
+    append(Y, [Element], Temp),
+    retract(temp_hulk(Y)), 
+    assertz(temp_hulk(Temp)), !.
+
 jarjestus(Hulga_nimi, List) :-
-    fail,
-    not(set_viimane(a)),
+    assertz(temp_hulk([])),
     not(loo_dynamic_hulk(Hulga_nimi)),
-    repeat,    
+    max(Hulga_nimi, Maksimum),
+    repeat,
     dynamic_hulk(Hulga_nimi, Element),
-    write(Element),write(' '),nl,
-    viimane(Viimane),
-    Viimane = Element.
+    min(dynamic_hulk, Hulga_nimi, Miinimum),
+    retract(dynamic_hulk(Hulga_nimi, Miinimum)),
+    add_to_list(Miinimum),
+    Maksimum = Miinimum, 
+    temp_hulk(List),
+    retract(temp_hulk(List)), !.
