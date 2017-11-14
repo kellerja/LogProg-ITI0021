d84dcb58db70e8b14c3a03e65fa764ee2a27c5b9
jakell <jakell@ttu.ee>
2017-11-14 13:34:18 +0200

    prax10

---
+++ PR10/prax10.pl
@@ -0,0 +1,14 @@
+liitlause --> lihtlause, lihtlause.
+liitlause --> liitlause, lihtlause.
+lihtlause --> nimisonafraas, tegusonafraas.
+nimisonafraas --> omadussonafraas, nimisona.
+omadussonafraas --> maarsona, omadussona.
+tegusonafraas --> tegusona, nimisonafraas.
+tegusonafraas --> nimisonafraas, tegusona.
+nimisona --> [].
+nimisona --> [kivile];[sammal];[uhkus];[raha];[volad].
+omadussona --> [veerevale].
+omadussona --> [].
+maarsona --> [upakile].
+maarsona --> [].
+tegusona --> [ei_kasva];[ajab];[tuleb];[laheb];[jaavad].
